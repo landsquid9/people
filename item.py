d84cb102f1a619e47d5ca2a5669d6b3784c30c96
landsquid9 <daniel.bandfield@ntlworld.com>
2017-09-23 21:05:08 +0100

    added food and eating action

---
+++ item.py
@@ -0,0 +1,6 @@
+""" Base class for a thinga person can materially posess """
+
+class Item:
+
+    def __init__(self):
+        self.weight = 1
--- person.py
+++ person.py
@@ -2,22 +2,36 @@ from random import randint
 
 import custom_logging as logging
 
+import food
+
+
 class Person:
 
-    def __init__(self, name, gender, locations, idleActions,knowledge=[]):
+    def __init__(self, name, gender, locations, idleActions, knowledge=[]):
         # chances out of 100
         self.EXPLORE_CHANCE = 10
+
+        # life signs
         self.name = name
-        self.hunger = 100
+        self.hunger = 50
         self.energy = 100
         self.mood = 100
+        self.dying = False
+        self.dyingMeter = 100
+        self.dead = False
+
+        # material
+        self.food = []
+        self.genFood(4)
+
         self.knowledge = knowledge
         self.globalLocations = locations
-        self.knownLocations = [self.globalLocations[0], self.globalLocations[1]]
+        self.knownLocations = [
+            self.globalLocations[0], self.globalLocations[1]]
         self.location = self.knownLocations[0]
         self.unknownLocations = self.globalLocations[2:]
         self.others = []
-        self.numActions = 4
+        self.numActions = 5
         self.messages = []
         self.gender = gender
         self.idleActions = idleActions
@@ -25,29 +39,60 @@ class Person:
     def setOthers(self, others):
         self.others = others
 
-
     def hourlyUpdate(self):
-
         """ Call each sim phase. Picks random action """
         # clear messages
         self.messages = []
 
+        # hunger etc
+        self.updateLifeSigns()
+
         # random action
-        ranAction = randint(0, self.numActions-1)
+        ranAction = randint(0, self.numActions - 1)
         if ranAction == 0 and len(self.knownLocations) > 1:
             # move
             self.move(self.getOtherLocation())
         elif ranAction == 1 and len(self.others) > 0:
             # converse
-            ranPerson = randint(0, len(self.others)-1)
+            ranPerson = randint(0, len(self.others) - 1)
             self.converse(self.others[ranPerson])
         elif ranAction == 2 and len(self.idleActions) > 0:
             logging.add("idle")
             self.idle()
         elif ranAction == 3 and len(self.unknownLocations) > 1:
             self.explore()
+        elif ranAction == 4 and self.hunger < 50:
+            self.eat()
 
         return self.messages
+
+    def updateLifeSigns(self):
+        if self.hunger > 0:
+            self.hunger -= 1
+        else:
+            self.dying = True
+
+        if self.dying:
+            if self.dyingMeter > 0:
+                self.dyingMeter -= 1
+            else:
+                self.dead = True
+
+    def genFood(self, numFood):
+        for i in range(numFood):
+            ranF = randint(0, 4)
+            if ranF == 0: self.food.append(food.Banana())
+            elif ranF == 1: self.food.append(food.Rice())
+            elif ranF == 2: self.food.append(food.Bacon())
+            elif ranF == 3: self.food.append(food.Waffle())
+            elif ranF == 4: self.food.append(food.Pineapple())
+
+    def eat(self):
+        self.message(self.name + " is hungry.")
+        self.hunger += self.food[-1].consume()
+        self.message(self.name + " ate a " + self.food[-1].name)
+        self.food.pop(-1)
+
 
     def explore(self):
         self.message(self.name + """ is curious and has started exploring
